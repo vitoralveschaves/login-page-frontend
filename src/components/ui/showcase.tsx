c007adfa23807dfc05b39cb1318ed9b0b7ddfdfd
vitoralveschaves <vitorchavesalv@gmail.com>
2024-09-24 22:01:53 -0300

    feat: add showcase component

---
+++ src/components/ui/showcase.tsx
@@ -0,0 +1,8 @@
+export const Showcase = () => {
+    return (
+        <div className="hidden lg:flex flex-1 justify-center items-center flex-col gap-4">
+            <h2 className="text-3xl text-black font-medium">You should, MoveIt!</h2>
+            <img src="/image.svg" alt="banner" />
+        </div>
+    )
+}
