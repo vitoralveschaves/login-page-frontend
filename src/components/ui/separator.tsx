55938ce32b4b41aefe61c9dd5ee2e964430794d8
vitoralveschaves <vitorchavesalv@gmail.com>
2024-09-24 22:01:37 -0300

    feat: add separator component

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,9 @@
+export const Separator = () => {
+    return (
+        <div className="w-full flex items-center gap-4 py-7">
+            <span className="h-[1px] bg-[#c2c2c2] flex flex-1"></span>
+            <span className="text-[#c2c2c2]">OR</span>
+            <span className="h-[1px] bg-[#c2c2c2] flex flex-1"></span>
+        </div>
+    )
+}
